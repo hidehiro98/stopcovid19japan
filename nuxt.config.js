ba9d270f16193edd71d64b98193d44ac9572b297
Hidehiro Nagaoka <kuinige@gmail.com>
2020-03-26 21:28:30 +0900

    Update og info

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: siteDesc },
-      { hid: 'og:image', property: 'og:image', content: '/ogp.png' }
+      { hid: 'og:site_name', property: 'og:site_name', content: siteName },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://stopcovid19japan.com/' },
+      { hid: 'og:title', property: 'og:title', content: siteName },
+      { hid: 'og:description', property: 'og:description', content: siteDesc },
+      { hid: 'og:image', property: 'og:image', content: '/ogp.png' },
+      { name: 'twitter:card', content: '/ogp.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
