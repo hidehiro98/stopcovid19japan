dfabb5f17fcf74b91907e22f3c2d34a870971f92
Hidehiro Nagaoka <kuinige@gmail.com>
2020-03-26 21:52:02 +0900

    Update twitter card

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,13 +12,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: siteDesc },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
       { hid: 'og:site_name', property: 'og:site_name', content: siteName },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'https://stopcovid19japan.com/' },
       { hid: 'og:title', property: 'og:title', content: siteName },
       { hid: 'og:description', property: 'og:description', content: siteDesc },
       { hid: 'og:image', property: 'og:image', content: '/ogp.png' },
-      { name: 'twitter:card', content: 'summary_large_image' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
