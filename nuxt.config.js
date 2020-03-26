eedb23b5b1f915cfafa7162e4790eee6b57601c5
Hidehiro Nagaoka <kuinige@gmail.com>
2020-03-26 22:25:52 +0900

    Update lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,9 @@ export default {
   */
   head: {
     title: siteName,
+    htmlAttrs: {
+      lang: 'ja'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
